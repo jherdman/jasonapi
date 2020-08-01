bc5c263b8745628415091dc4930f3b6eebe68530
James Herdman <james.herdman@gmail.com>
2020-07-31 20:01:02 -0400

    Set Yourself Up for Success

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Jasonapi.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: aliases(),
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -21,8 +23,29 @@ defmodule Jasonapi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+      plt_add_deps: :app_tree,
+      ignore_warnings: ".dialyzer_ignore.exs"
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: [
+        "compile --warnings-as-errors",
+        "format --check-formatted",
+        "credo",
+        "dialyzer"
+      ]
     ]
   end
 end
